ecfdcb39f16da3e21f4e2a9f3149000016d87010
Mario Torres Jr <torres.mario590@gmail.com>
2023-03-07 11:16:33 -0600

    create test for POST and DELETE routes for dogs api

--- index.test.js
+++ index.test.js
@@ -34,3 +34,51 @@ describe('Endpoints', () => {
         });
     });
 });
+
+describe('POST /dogs', () => {
+    it('should create a dog in the database with the entered data', async () => {
+        const testDogData = {
+            breed: 'Poodle',
+            name: 'Sasha',
+            color: 'black',
+            description: 'Sasha is a beautiful black pooodle mix.  She is a great companion for her family.'
+        };
+        const response = await request(app).post('/dogs').send(testDogData);
+        expect(response.status).toBe(200);
+        expect(response.body).toBeDefined();
+        expect(response.body).toEqual(expect.objectContaining(testDogData));
+    });
+    it('Assert that the dog data from the DB matches the request body', async () => {
+           const testDogData = {
+        breed: 'Poodle',
+        name: 'Sasha',
+        color: 'black',
+        description: 'Sasha is a beautiful black pooodle mix.  She is a great companion for her family.'
+    };
+        const response = await request(app).post('/dogs').send(testDogData);
+        
+        const dog = response.body;
+        const datab_Dog = await Dog.findOne({where: testDogData});
+        expect(dog.name).toBe(datab_Dog.name);
+        expect(dog.breed).toBe(datab_Dog.breed);
+        expect(dog.color).toBe(datab_Dog.color);
+        expect(dog.description).toBe(datab_Dog.description);
+    });
+});
+
+describe('DELETE /dogs', () => {
+    it('should delete a dog by id', async () => {
+        
+        const response = await request(app).delete('/dogs/1');
+        
+        expect(response.status).toBe(200);
+        
+        expect(response.text).toEqual('deleted dog with id 1');
+    });
+    it('Assert that the dog data is null', async () => {
+        const response = await request(app).delete('/dogs/1');
+        
+        const datab_Dog = await Dog.findByPk(1);
+        expect(datab_Dog).toBeNull();
+    });
+});
